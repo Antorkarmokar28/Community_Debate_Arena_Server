679b8bea46161a2d50e1dbd7e8506ef57a458899
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-07-18 17:47:27 +0600

    This project in notfound custom middleware create for if any rount not found user request

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 // using parser
 app.use(express());
@@ -8,9 +9,11 @@ app.use(cors());
 const exicuteServer = async (req: Request, res: Response) => {
   res.send({
     success: true,
-    message: "The Community Debate Arena Server is Running",
+    message: 'The Community Debate Arena Server is Running',
   });
 };
-app.get("/", exicuteServer);
+app.get('/', exicuteServer);
+// notFound middleware include here for if any route not found then exicute the middleware
+app.use(notFound);
 
 export default app;
